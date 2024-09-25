abfabd807bb5a4b45f5e009187beda8c13862528
PedroBdS <pedrobottari@gmail.com>
2024-09-25 16:12:30 -0300

    atualizações

--- funcoes.py
+++ funcoes.py
@@ -3,6 +3,8 @@ import time
 import keyboard
 from PIL import ImageGrab, Image
 import numpy as np
+import pytesseract
+
 
 matriz_mestre = [
     (0,0,1,0,0,0,0,0),
@@ -89,85 +91,6 @@ def checar_cor(coordenada, cor):
     else:
         return False
 
-def iniciar_shaiya(conta):
-    time.sleep(0.2)
-
-    # Pressiona a tecla Windows
-    keyboard.press('windows')
-    keyboard.release('windows')
-
-    # Espera um pouco para o menu iniciar abrir
-    time.sleep(0.2)
-
-    # Digita "fawkes"
-    pyautogui.write('fawkes')
-
-    # Pressiona Enter
-    pyautogui.press('enter')
-
-    # PLAY Posição do mouse: (670, 552), Cor do pixel: (236, 56, 36)
-    esperar_cor_e_clicar((670, 552),(236, 56, 36), ingame=False, nome='PLAY')
-
-    # Connect Posição do mouse: (1841, 1021), Cor do pixel: (233, 69, 53)
-    esperar_cor_e_clicar((1841, 1021), (233, 69, 53), ingame=False, nome='Connect')
-
-    # Login Posição do mouse: (1006, 819), Cor do pixel: (22, 21, 22)
-    esperar_cor_e_clicar((1006, 819), (22, 21, 22), nome='Login')
-    pyautogui.write(conta)
-
-    # Senha Posição do mouse: (996, 875), Cor do pixel: (23, 24, 27)
-    esperar_cor_e_clicar((996, 875), (23, 24, 27), nome='Senha')
-    pyautogui.write('Bolinho123321!')
-
-    # Entrar Posição do mouse: (991, 965), Cor do pixel: (68, 33, 33)
-    esperar_cor_e_clicar((991, 965), (68, 33, 33), nome='Entrar')
-
-    time.sleep(0.3)
-
-    # Por favor espere um momento antes de iniciar novamente a sessão.
-    # Posição do mouse: (1023, 509), Cor do pixel: (34, 34, 37)
-
-    if checar_cor((1023, 509), (34, 34, 37)):
-        return False
-
-    # A conexão com o servidor foi encerrada.
-    # Posição do mouse: (1077, 504), Cor do pixel: (28, 24, 24)
-
-    if checar_cor((1077, 504), (28, 24, 24)):
-        return False
-
-    # Servidor Posição do mouse: (951, 658), Cor do pixel: (24, 24, 24)
-    esperar_cor_e_clicar((951, 658), (24, 24, 24), nome='Servidor')
-                         
-    # OK Posição do mouse: (921, 971), Cor do pixel: (63, 32, 32)
-    esperar_cor_e_clicar((921, 971), (63, 32, 32), nome='OK')
-
-    # Esperar tela personagens Posição do mouse: (803, 455), Cor do pixel: (11, 8, 5)
-    esperar_cor((803, 455), (11, 8, 5), nome='Esperar tela personagens')
-
-    # P1 Posição do mouse: (468, 255), Cor do pixel: (27, 22, 15)
-    # esperar_cor_e_clicar((468, 255), nome='P1')
-
-    # P2 Posição do mouse: (468, 378), Cor do pixel: (30, 12, 41)
-    esperar_cor_e_clicar((468, 378), nome='P2')
-
-    # P3 Posição do mouse: (468, 508), Cor do pixel: (17, 19, 26)
-    # esperar_cor_e_clicar((468, 508), nome='P3')
-
-    # P4 Posição do mouse: (468, 634), Cor do pixel: (16, 23, 21)
-    # esperar_cor_e_clicar((468, 634), nome='P4')
-
-    # P5 Posição do mouse: (468, 768), Cor do pixel: (18, 21, 28)
-    # esperar_cor_e_clicar((468, 768), nome='P5')
-
-    # Início do jogo Posição do mouse: (1385, 1024), Cor do pixel: (250, 249, 247)
-    esperar_cor_e_clicar((1385, 1024), (250, 249, 247), nome='')
-
-    # Confirmar ingame Posição do mouse: (939, 1061), Cor do pixel: (251, 239, 185)
-    esperar_cor((939, 1061), (251, 239, 185), nome='')
-
-    return True
-
 def comparar_img(img1, img2):
     
     # Verificar se as dimensões são diferentes
@@ -202,26 +125,14 @@ def identificar_erros():
 
 def abrir_leilao():
 
-    # Posição do mouse: (595, 246), Cor do pixel: (73, 73, 72)
-    # Posição do mouse: (597, 249), Cor do pixel: (35, 34, 36)
-    # Posição do mouse: (596, 253), Cor do pixel: (24, 24, 26)
-
-    # botao fechar Posição do mouse: (1324, 260), Cor do pixel: (86, 35, 37)
-
-    # Abrir leilão Posição do mouse: (1422, 1036), Cor do pixel: (249, 178, 102) 
-
-
     if not(checar_cor((595, 246), (73, 73, 72)) and checar_cor((597, 249), (35, 34, 36))):
         click_ingame((1422, 1036))
-
-    if not(checar_cor((595, 246), (73, 73, 72)) and checar_cor((597, 249), (35, 34, 36))):
-        click_ingame((1422, 1036))
-
-    if not(checar_cor((595, 246), (73, 73, 72)) and checar_cor((597, 249), (35, 34, 36))):
-        click_ingame((1422, 1036))
+        time.sleep(0.3)
+        click_ingame((1250, 312))
 
     if not(checar_cor((595, 246), (73, 73, 72)) and checar_cor((597, 249), (35, 34, 36))):
         print('Não foi possível abrir o leilão')
+        return False
 
 def capturar_tela(coordenada1, coordenada2, arquivo_saida=False):
     # Obter as coordenadas de referência
@@ -257,7 +168,15 @@ def cor_do_nome(imagem):
     print(f'Falha ao identificar a cor do nome.')
     return (0, 0, 0)
 
-def atualizar_leilao():
+def checar_novo_item():
+
+    # novo_nome = ler_nome()
+    novo_valor = ler_valor()
+
+def atualizar_leilao(nome=False, valor=False):
+    if not nome:
+        click_ingame((1250, 312))
+        return
     click_ingame((1250, 312))
 
 def formatar_nome(image, cor_rgb):
@@ -358,9 +277,14 @@ def converte_para_numero(matriz_impar, matriz_par):
     return int(resposta)
                 
 def numero_para_valor(gold, silver, copper):
-    return gold*10000000000 + silver*1000000 + copper
+    return gold*1000000000 + silver*100000 + copper
 
-def print_para_valor(imagem):
+def ler_valor():
+
+    click_ingame((931, 444))
+
+    imagem = capturar_tela((1182, 398), (1287, 406))
+
     imagem = formatar_nome(imagem, (255,255,255))
     imagem = imagem.convert('1')   
     matriz = np.array(imagem, dtype=int)
@@ -436,14 +360,23 @@ def print_para_valor(imagem):
 
     return valor
 
-def printar_nome_e_valor():
-    # clicar no segundo item
-    esperar_cor_e_clicar((931, 444))
+def extrair_texto_imagem(imagem):
+    imagem.show()
+    # Usa o pytesseract para extrair o texto
+    texto = pytesseract.image_to_string(imagem, lang='por')  # 'lang' define o idioma, aqui é português
+    
+    return texto
 
-    # printar o nome
+# clicar no segundo item
+    # esperar_cor_e_clicar((931, 444))
+
+def ler_nome():
     imagem_nome = capturar_tela((808, 376), (1025, 390))
+    cor_nome = cor_do_nome(imagem_nome)
+    img_nome_tratado = formatar_nome(imagem_nome, cor_nome)
+    nome = extrair_texto_imagem(img_nome_tratado)
 
-    # printar o gold
-    imagem_gold = capturar_tela((1182, 398), (1287, 406))
+    img_nome_tratado.save('teste_lerNome.png')
 
-    return imagem_nome, imagem_gold
+    return nome
+    
---
+++ teste.py
@@ -0,0 +1,12 @@
+from funcoes import *
+
+abrir_leilao()
+
+time.sleep(0.2)
+
+nome = ler_nome()
+
+valor = ler_valor()
+
+print(f'\nNome: {nome}\nValor: {valor}')
+
